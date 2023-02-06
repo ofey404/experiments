fda3a84520671c79932bcd571e111fc438d91da7
Ofey Chan <ofey206@gmail.com>
2023-02-06 10:43:54 +0800

    refactor: move vault out of playground

---
+++ playground/kubeflow/README.md
@@ -0,0 +1,4 @@
+# kubeflow playground
+
+This directory demonstrates multiple ways to run a [kubeflow/kubeflow](https://github.com/kubeflow/kubeflow)
+on a k8s cluster.
---
+++ playground/kubeflow/commands.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# set -x             # for debug
+set -euo pipefail  # fail early
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+##############################################
+# Don't run. This file is a note for commands.
+##############################################
