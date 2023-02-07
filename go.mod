b840ed960219b04f3762d9934a06efb26b289d17
Ofey Chan <ofey206@gmail.com>
2023-02-07 11:49:57 +0800

    stage: createJob

--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require (
 	github.com/Shopify/sarama v1.37.2
 	github.com/confluentinc/confluent-kafka-go v1.9.2
 	github.com/hashicorp/vault/api v1.8.3
+	github.com/kubeflow/common v0.4.3
 	github.com/kubeflow/training-operator v1.5.0
 	github.com/pkg/profile v1.7.0
 	github.com/segmentio/kafka-go v0.4.38
 	github.com/zeromicro/go-zero v1.4.3
+	k8s.io/api v0.26.1
 	k8s.io/apimachinery v0.26.1
 	k8s.io/client-go v0.26.1
 )
@@ -63,7 +65,6 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
-	github.com/kubeflow/common v0.4.3 // indirect
 	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
@@ -102,7 +103,6 @@ require (
 	gopkg.in/square/go-jose.v2 v2.5.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	k8s.io/api v0.26.1 // indirect
 	k8s.io/klog/v2 v2.80.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20221012153701-172d655c2280 // indirect
 	k8s.io/utils v0.0.0-20221128185143-99ec85e7a448 // indirect
--- playground/kubeflow/main.go
+++ playground/kubeflow/main.go
@@ -4,8 +4,14 @@ import (
 	"context"
 	"flag"
 	"fmt"
+
+	v1 "github.com/kubeflow/training-operator/pkg/apis/kubeflow.org/v1"
+	v12 "k8s.io/api/core/v1"
+
 	"os"
 	"path/filepath"
+
+	commonv1 "github.com/kubeflow/common/pkg/apis/common/v1"
 
 	clientv1 "github.com/kubeflow/training-operator/pkg/client/clientset/versioned"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -20,7 +26,30 @@ func _must(err error) {
 	}
 }
 
+func _int32Pointer(i int) *int32 {
+	i32Value := int32(i)
+	return &i32Value
+}
+
+const (
+	list = iota
+	create
+)
+
+var feature = create
+
 func main() {
+	kfcli := createClient()
+
+	switch feature {
+	case list:
+		listJobs(kfcli)
+	case create:
+		createJob(kfcli, "pytorch-simple-affinity-by-client")
+	}
+}
+
+func createClient() *clientv1.Clientset {
 	var kubeconfig *string
 	if home := homedir.HomeDir(); home != "" {
 		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
@@ -35,8 +64,74 @@ func main() {
 
 	kfcli, err := clientv1.NewForConfig(config)
 	_must(err)
+	return kfcli
+}
 
-	jobs, err := kfcli.KubeflowV1().PyTorchJobs("kubeflow").List(context.TODO(), metav1.ListOptions{})
+func createJob(kfcli *clientv1.Clientset, name string) {
+	job, err := kfcli.KubeflowV1().PyTorchJobs("kubeflow").Create(
+		context.TODO(),
+		&v1.PyTorchJob{
+			TypeMeta: metav1.TypeMeta{
+				Kind:       "PyTorchJob",
+				APIVersion: "kubeflow.org/v1",
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      name,
+				Namespace: "kubeflow",
+			},
+			Spec: v1.PyTorchJobSpec{
+				PyTorchReplicaSpecs: map[commonv1.ReplicaType]*commonv1.ReplicaSpec{
+					"Master": &commonv1.ReplicaSpec{
+						Replicas: _int32Pointer(1),
+						Template: v12.PodTemplateSpec{
+							Spec: v12.PodSpec{
+								Containers: []v12.Container{
+									{
+										Name:            "pytorch",
+										Image:           "docker.io/kubeflowkatib/pytorch-mnist:v1beta1-45c5727",
+										ImagePullPolicy: v12.PullAlways,
+										Command: []string{
+											"python3",
+											"/opt/pytorch-mnist/mnist.py",
+											"--epochs=1",
+										},
+									},
+								},
+							},
+						},
+						RestartPolicy: commonv1.RestartPolicyOnFailure,
+					},
+					"Worker": &commonv1.ReplicaSpec{
+						Replicas: _int32Pointer(2),
+						Template: v12.PodTemplateSpec{
+							Spec: v12.PodSpec{
+								Containers: []v12.Container{
+									{
+										Name:            "pytorch",
+										Image:           "docker.io/kubeflowkatib/pytorch-mnist:v1beta1-45c5727",
+										ImagePullPolicy: v12.PullAlways,
+										Command: []string{
+											"python3",
+											"/opt/pytorch-mnist/mnist.py",
+											"--epochs=1",
+										},
+									},
+								},
+							},
+						},
+						RestartPolicy: commonv1.RestartPolicyOnFailure,
+					},
+				},
+			},
+		},
+		metav1.CreateOptions{},
+	)
 	_must(err)
-	fmt.Println(jobs)
+	fmt.Println(job)
+}
+
+func listJobs(kfcli *clientv1.Clientset) {
+	jobList, err := kfcli.KubeflowV1().PyTorchJobs("kubeflow").List(context.TODO(), metav1.ListOptions{})
+	_must(err)
+	fmt.Println(jobList)
 }
