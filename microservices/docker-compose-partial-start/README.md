4fc57e578ded4af50466754c78ae5962d2dc0d6c
Ofey Chan <ofey206@gmail.com>
2023-02-20 16:51:32 +0800

    feat: docker-compose partial start

---
+++ microservices/docker-compose-partial-start/README.md
@@ -0,0 +1,6 @@
+# docker-compose partial start
+
+Requirements:
+
+1. service `useless_dependency` should not start.
+2. `go run ...` can access service `pingpong` from host.
---
+++ microservices/docker-compose-partial-start/run.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# set -x             # for debug
+set -euo pipefail  # fail early
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR"
+
+echo "## SERVICE useless_dependency should not start."
+docker-compose up curl
