dd29520b036189dbe6517c9785a2f70e9f7c7575
Ofey Chan <ofey206@gmail.com>
2023-03-23 15:09:45 +0800

    stage: lambda_handler

--- cloud/aws_lambda/main.tf
+++ cloud/aws_lambda/main.tf
@@ -13,7 +13,47 @@ provider "aws" {
   region = "us-west-2"
 }
 
-#####################################################################
+resource "aws_lambda_function" "cluster_down" {
+  filename      = "lambda_function_payload.zip"
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  function_name = "cluster_down"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler = "lambda.lambda_handler"
+
+  runtime = "python3.7"
+
+  environment {
+      variables = {
+        "MODE" = "DOWN"
+      }
+  }
+}
+
+resource "aws_cloudwatch_event_rule" "daily_trigger_8pm" {
+  name                = "daily_trigger_8pm"
+  description         = "Triggered once every day at 8PM SGT"
+  schedule_expression = "cron(0 12 * * *)" # This will trigger at 8:00 PM SGT (12 PM UTC)
+}
+
+resource "aws_cloudwatch_event_target" "cluster_down" {
+  rule      = aws_cloudwatch_event_rule.daily_trigger_8pm.name
+  target_id = "cluster_down"
+  arn       = aws_lambda_function.cluster_down.arn
+}
+
+# https://stackoverflow.com/questions/35895315/use-terraform-to-set-up-a-lambda-function-triggered-by-a-scheduled-event-source
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
+    statement_id = "AllowExecutionFromCloudWatch"
+    action = "lambda:InvokeFunction"
+    function_name = aws_lambda_function.cluster_down.function_name
+    principal = "events.amazonaws.com"
+    source_arn = aws_cloudwatch_event_rule.daily_trigger_8pm.arn
+}
+
+# ###################################################################
+# Role and permissions for lambda
+# ###################################################################
 
 resource "aws_iam_role" "iam_for_lambda" {
   name               = "iam_for_lambda"
@@ -44,53 +84,8 @@ resource "aws_iam_role_policy_attachment" "eks" {
   role       = aws_iam_role.iam_for_lambda.name
 }
 
-#####################################################################
-
 data "archive_file" "lambda" {
   type        = "zip"
   source_file = "lambda.py"
   output_path = "lambda_function_payload.zip"
 }
-
-resource "aws_lambda_function" "test_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
-  filename      = "lambda_function_payload.zip"
-  function_name = "lambda_function_name"
-  role          = aws_iam_role.iam_for_lambda.arn
-  handler = "lambda.lambda_handler"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "python3.7"
-
-  environment {
-      variables = {
-      "TEST" = "test"
-      }
-  }
-}
-
-#####################################################################
-
-# Create a CloudWatch Event Rule
-resource "aws_cloudwatch_event_rule" "timer" {
-    name                = "timer"
-    description         = "Every 1 minute"
-    schedule_expression = "rate(1 minute)"
-}
-
-# resource "aws_cloudwatch_event_target" "my_cloudwatch_event_target" {
-#   rule      = aws_cloudwatch_event_rule.timer.name
-#   target_id = "timer-event-target"
-#   arn       = aws_lambda_function.test_lambda.arn
-# }
-
-# https://stackoverflow.com/questions/35895315/use-terraform-to-set-up-a-lambda-function-triggered-by-a-scheduled-event-source
-# resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
-#     statement_id = "AllowExecutionFromCloudWatch"
-#     action = "lambda:InvokeFunction"
-#     function_name = aws_lambda_function.test_lambda.function_name
-#     principal = "events.amazonaws.com"
-#     source_arn = aws_cloudwatch_event_rule.timer.arn
-# }
