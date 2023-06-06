6e37125f3acda2c96c304e95cb277daa99429c19
Ofey Chan <ofey206@gmail.com>
2023-06-06 16:11:57 +0800

    stage: fluid

---
+++ cloud/fluid/README.md
@@ -0,0 +1,15 @@
+# fluid
+
+This directory is a playground of fluid,
+[fluid-cloudnative/fluid](https://github.com/fluid-cloudnative/fluid)
+
+> Fluid, elastic data abstraction and acceleration for
+> 
+> BigData/AI applications in cloud. (Project under CNCF)
+
+[AI ACCELERATION WHITE PAPER 2021 | Alibaba Cloud](https://whitepaperseries.com/wp-content/uploads/2022/05/101622_AI-Acceleration-Whitepaper.pdf)
+has introduced fluid, as part of the full AI acceleration stack in AliCloud.
+
+## Get started
+
+I follow this guide: [Get Started With Fluid](https://github.com/fluid-cloudnative/fluid/blob/master/docs/en/userguide/get_started.md)
---
+++ cloud/fluid/commands.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# set -x             # for debug
+set -euo pipefail  # fail early
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR"
+
+##############################################
+# Don't run. This file is a note for commands.
+##############################################
+
+kubectl create ns fluid-system
+
+helm repo add fluid https://fluid-cloudnative.github.io/charts
+helm repo update
+
+helm install fluid fluid/fluid
+
+kubectl get po -n fluid-system
+
+kubectl create -f dataset.yaml -f runtime.yaml -f app.yaml
+
+##############################################
+# Inside the container
+##############################################
+
+kubectl exec -it demo-app -- bash
+
+du -sh /data/spark/spark-3.4.0/spark-3.4.0-bin-without-hadoop.tgz
+# 287M    /data/spark/spark-3.4.0/spark-3.4.0-bin-without-hadoop.tgz
+time cp /data/spark/spark-3.4.0/spark-3.4.0-bin-without-hadoop.tgz /dev/null
+# real    0m46.987s
+# user    0m0.003s
+# sys     0m0.948s
+
+##############################################
+# Recreate the application
+##############################################
+
+kubectl delete -f app.yaml && kubectl create -f app.yaml
+
+kubectl exec -it demo-app -- bash
+time cp /data/spark/spark-3.4.0/spark-3.4.0-bin-without-hadoop.tgz /dev/null
+# real    0m0.543s
+# user    0m0.000s
+# sys     0m0.094s
