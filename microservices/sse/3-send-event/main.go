5da3ca02d53b582a105e9a0f90c22db616dacfce
Ofey Chan <ofey206@gmail.com>
2023-06-19 10:51:38 +0800

    stage: 3 send event

---
+++ microservices/sse/3-send-event/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
+
+func escape(input string) string {
+	return strings.ReplaceAll(input, "\n", "\\n")
+}
+
+func multi_line_data() string {
+	return escape(
+		fmt.Sprintf("Multiline data\ntime:\n%s\n", time.Now().Format(time.RFC3339)),
+	)
+}
+
+func main() {
+	http.HandleFunc("/", handle)
+	fmt.Printf("Listening on localhost:8080\n")
+	_ = http.ListenAndServe(":8080", nil)
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	// Set headers for long polling connection
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Send initial data to the client
+	_, _ = fmt.Fprintf(w, "data: %s\n\n", multi_line_data())
+
+	counter := 0
+	// Continuously send updates to the client
+	for {
+		time.Sleep(1 * time.Second)
+		if counter%10 == 0 {
+			_, _ = fmt.Fprintf(w, "event: testEvent\ndata: %s\n\n", multi_line_data())
+		} else {
+			_, _ = fmt.Fprintf(w, "data: %s\n\n", multi_line_data())
+		}
+		w.(http.Flusher).Flush()
+		counter += 1
+	}
+}
