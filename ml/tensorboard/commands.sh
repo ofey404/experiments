8a2c12b333d3da4787b2956564d213883cb12fe7
Weiwen Chen <ofey206@gmail.com>
2023-06-26 10:01:49 +0000

    stage: tensorboard

--- ml/tensorboard/commands.sh
+++ ml/tensorboard/commands.sh
@@ -9,3 +9,7 @@ cd "$SCRIPT_DIR"
 # Don't run. This file is a note for commands.
 ##############################################
 
+pip install tensorboard
+python train.py
+
+docker run -it --rm -p 0.0.0.0:6000:6006 -v /tensorboard_logs/:$(pwd)/runs/ tensorflow/tensorflow:2.5.1 /usr/local/bin/tensorboard --logdir=/tensorboard_logs/ --host 0.0.0.0
