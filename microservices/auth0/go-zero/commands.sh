08ea01ad40d6c718eed09498552f44c219327ef2
Ofey Chan <ofey206@gmail.com>
2023-07-15 16:52:02 +0800

    stage: frontend and deploy skeleton

---
+++ microservices/auth0/go-zero/commands.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# set -x             # for debug
+set -euo pipefail  # fail early
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR"
+
+##############################################
+# Don't run. This file is a note for commands.
+##############################################
+
+# build image auth0-go-zero-backend:latest
+backend/build.sh
