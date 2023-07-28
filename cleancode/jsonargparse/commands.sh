ace321d3ec72469a69d9903340ba9ca9c71c70a6
Ofey Chan <ofey206@gmail.com>
2023-07-28 17:43:04 +0800

    clean code: more jsonargparse
    Former-commit-id: 045103af62d0ed8fc84e3e4d5c07aa38adf2753d

--- cleancode/jsonargparse/commands.sh
+++ cleancode/jsonargparse/commands.sh
@@ -9,7 +9,7 @@ cd "$SCRIPT_DIR"
 # Don't run. This file is a note for commands.
 ##############################################
 
-pip install jsonargparse[signatures]==4.23.0 pydantic
+pip install jsonargparse==4.23.0 pydantic
 
 python 1-parser.py 
 # usage: 1-parser.py [-h] [--config CONFIG] [--print_config[=flags]] [--prize PRIZE] name
@@ -43,4 +43,37 @@ python 1-parser.py --config 1-parser.yaml overrideFromShell --prize 10000
 python 2-nested.py --config 2-nested.yaml 
 # fromNestedYaml won 404€!
 # Contact info: Contact(city='mountainView', email='noreply@gmail.com', phone=['1234567890', '0987654321'])
+
+python 2-nested.py --help
+# /home/ofey/.local/lib/python3.10/site-packages/pydantic/_migration.py:283: UserWarning: `pydantic.utils:Representation` has been removed. We are importing from `pydantic.v1.utils:Representation` instead.See the migration guide for more details: https://docs.pydantic.dev/latest/migration/
+#   warnings.warn(
+# usage: 2-nested.py [-h] [--config CONFIG] [--print_config[=flags]] [--contact CONFIG] --contact.city CITY --contact.email EMAIL
+#                    --contact.phone PHONE [--contact.meta CONFIG] --contact.meta.m1 M1 [--prize PRIZE]
+#                    name
+# 
+# Prints the prize won by a person.
+# 
+# positional arguments:
+#   name                  Name of winner. (required, type: str)
+# 
+# options:
+#   -h, --help            Show this help message and exit.
+#   --config CONFIG       Path to a configuration file.
+#   --print_config[=flags]
+#                         Print the configuration after applying all other arguments and exit. The optional flags customizes the output and are one or
+#                         more keywords separated by comma. The supported flags are: comments, skip_default, skip_null.
+#   --prize PRIZE         Amount won. (type: int, default: 100)
+# 
+# Contact(city: str, email: str, phone: List[str], meta: __main__.Meta):
+#   --contact CONFIG      Path to a configuration file.
+#   --contact.city CITY   (required, type: str)
+#   --contact.email EMAIL
+#                         (required, type: str)
+#   --contact.phone PHONE, --contact.phone+ PHONE
+#                         (required, type: List[str])
+# 
+# Meta(m1: str):
+#   --contact.meta CONFIG
+#                         Path to a configuration file.
+#   --contact.meta.m1 M1  (required, type: str)
 
---
+++ ml/torchrun/README.md
@@ -0,0 +1,3 @@
+# torchrun
+
+This is my playground of [torchrun](https://pytorch.org/docs/stable/elastic/run.html)
