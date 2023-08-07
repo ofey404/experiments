9c3d8b53b4452eac6e40f1aea83038c8fd7cbc1c
Ofey Chan <ofey206@gmail.com>
2023-08-07 09:51:55 +0800

    stage: presign-upload
    Former-commit-id: 8a1d3e38887506aa87f89b202ec4223d0f395a28

--- cloud/volcengine/tossdk/1-list.py
+++ cloud/volcengine/tossdk/1-list.py
@@ -1,28 +1,13 @@
-import os 
-import tos
+from client import new_client, BUCKET
 
-try:
-    import crcmod._crcfunext
-except ImportError:
-    raise Exception("TOS requires the crcmod C extension, or the slow python-native checksum would severely hurt upload performance")
-
-# Accesskey和Secretkey可在用户火山引擎账号中查找。
-ak = os.environ["VOLCENGINE_ACCESS_KEY"]
-sk = os.environ["VOLCENGINE_SECRET_KEY"]
-bucket = os.environ["VOLCENGINE_TEST_BUCKET"]
-
-# your endpoint 和 your region 填写Bucket 所在区域对应的Endpoint。# 以华北2(北京)为例，your endpoint 填写 tos-cn-beijing.volces.com，your region 填写 cn-beijing。 
-endpoint = "tos-cn-beijing.volces.com"
-region = "cn-beijing"
-
-client = tos.TosClientV2(ak, sk, endpoint, region)
+client = new_client()
 
 # list has a upper limit 1000, by default
-out = client.list_objects(bucket)
+out = client.list_objects(BUCKET)
 for o in out.contents:
     print(o.key)
 
 while out.is_truncated:
-    out = client.list_objects(bucket, marker=out.next_marker)
+    out = client.list_objects(BUCKET, marker=out.next_marker)
     for ob in out.contents:
         print(ob.key)
--- cloud/volcengine/tossdk/commands.sh
+++ cloud/volcengine/tossdk/commands.sh
@@ -10,3 +10,13 @@ cd "$SCRIPT_DIR"
 ##############################################
 
 python -m pip install tos
+
+source .env
+
+python 1-list.py 
+
+# generate presigned URL with {bucket key}
+python 2-presign-generate.py test-sdk/2-presign-generate.txt
+
+# upload with {file path} {presign url}
+python 3-presign-upload.py README.md 'https://cloud-platform-manifests.tos-cn-beijing.volces.com/test-sdk/2-presign-generate.txt?X-Tos-Algorithm=TOS4-HMAC-SHA256&X-Tos-Credential=AKLTZWE5ZjZkMmRlNDFiNGIyYmIwMzAwMGRiZjNlNzQzM2Q%2F20230804%2Fcn-beijing%2Ftos%2Frequest&X-Tos-Date=20230804T022258Z&X-Tos-Expires=3600&X-Tos-SignedHeaders=host&X-Tos-Signature=814323390b1d1008c554c04b617798d80a20a0fa4d0e33ee7503a1faad7a815c'
