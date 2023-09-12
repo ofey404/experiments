34919396869f3c9c816b0a189e00741804d9e981
Ofey Chan <ofey206@gmail.com>
2023-09-12 18:17:24 +0800

    stage: sklearn-iris.default.example.com
    Former-commit-id: 13823c9806241e2efeef98d49783f8e8bedc7eb8

---
+++ cloud/kserve/README.md
@@ -0,0 +1,7 @@
+# kserve
+
+[GitHub - kserve/kserve: Standardized Serverless ML Inference Platform on Kubernetes](https://github.com/kserve/kserve)
+
+[Serverless installation - KServe Documentation Website](https://kserve.github.io/website/0.11/admin/serverless/serverless/)
+
+
--- cloud/kserve/commands.sh
+++ cloud/kserve/commands.sh
@@ -9,4 +9,17 @@ cd "$SCRIPT_DIR"
 # Don't run. This file is a note for commands.
 ##############################################
 
-# https://github.com/kserve/kserve
+kind create cluster -n kserve
+
+# I use the `quickstart` configuration
+# https://kserve.github.io/website/0.11/get_started/#install-the-kserve-quickstart-environment
+curl -s "https://raw.githubusercontent.com/kserve/kserve/release-0.11/hack/quick_install.sh" | bash
+
+# First inference service
+# https://kserve.github.io/website/master/get_started/first_isvc/#run-your-first-inferenceservice
+kubectl create namespace kserve-test
+kubectl apply -f manifests/first-inference-service.yaml
+
+kubectl -n istio-system port-forward svc/istio-ingressgateway 8080:80
+curl -v -H "Host: sklearn-iris.default.example.com" -H "Content-Type: application/json" "http://localhost:8080/v1/models/sklearn-iris:predict" -d @./iris-input.json
+# {"predictions":[1,1]}
