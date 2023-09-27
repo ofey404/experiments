5a1303bc3d6c26299905f489cb194dbc08accb60
Ofey Chan <ofey206@gmail.com>
2023-09-27 14:05:47 +0800

    stage: python example
    Former-commit-id: 24c66f5e40f2262b47e76027d428ebc2ca97f22e

--- microservices/grpc/helloworld/README.md
+++ microservices/grpc/helloworld/README.md
@@ -2,4 +2,7 @@
 
 This is a modified quickstart example, emphasised the cross-language ability of protobuf.
 
-Modified from https://github.com/grpc/grpc-go/examples/helloworld
+Examples are from:
+
+- Go: https://github.com/grpc/grpc-go/examples/helloworld
+- Python: https://github.com/grpc/grpc/examples/python/helloworld
--- microservices/grpc/helloworld/commands.sh
+++ microservices/grpc/helloworld/commands.sh
@@ -8,3 +8,13 @@ cd "$SCRIPT_DIR"
 ##############################################
 # Don't run. This file is a note for commands.
 ##############################################
+
+# to use python protobuf
+python -m pip install grpcio grpcio-tools google
+
+
+# pick commands from them
+python python/greeter_server.py
+python python/greeter_client.py
+go run greeter_server/main.go
+go run greeter_client/main.go
