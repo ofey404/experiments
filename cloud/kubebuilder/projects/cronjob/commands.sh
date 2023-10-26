bfbf0ca1a672bf04bcf81afae149c8d6b75480c3
Ofey Chan <ofey206@gmail.com>
2023-10-26 15:16:19 +0800

    stage: a working cronjob

--- cloud/kubebuilder/projects/cronjob/commands.sh
+++ cloud/kubebuilder/projects/cronjob/commands.sh
@@ -18,3 +18,17 @@ rm go.mod go.sum
 
 kubebuilder create api --group batch --version v1 --kind CronJob
 make manifests
+
+# Oh shit, the whole lotta tutorial is not in a good code style.
+# The controller.go is a top-down spaghetti script. Very hard to read.
+#
+# So never follow the tutorial, just copy the source code from here:p
+# https://github.com/kubernetes-sigs/kubebuilder/blob/master/docs/book/src/cronjob-tutorial/testdata/project/internal/controller/cronjob_controller.go
+
+# Here is another kubebuilder bug, we should add crd:maxDescLen=0 in Makefile:
+#
+# metadata.annotations: Too long: must have at most 262144 bytes #2556
+# https://github.com/kubernetes-sigs/kubebuilder/issues/2556
+
+# finally... Apply our test cronjob.
+kubectl apply -f examples/cronjob.yaml
