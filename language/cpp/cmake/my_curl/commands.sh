910bc498e1ffe20f45816e4c689e6ca23da6fbcb
Ofey Chan <ofey206@gmail.com>
2023-11-18 14:57:45 +0800

    cmake/my_curl: everything is set

--- language/cpp/cmake/my_curl/commands.sh
+++ language/cpp/cmake/my_curl/commands.sh
@@ -18,3 +18,21 @@ mkdir cmake-build-debug
 cd cmake-build-debug
 cmake ..
 make
+
+# run the outputs
+./src/my_curl
+# <!doctype html><html itemscope="" itemtype="http://schema.org/WebPage" lang="en-SG"><head>
+# ...
+
+# run the tests
+./tests/my_curl_test
+# [==========] Running 1 test from 1 test suite.
+# [----------] Global test environment set-up.
+# [----------] 1 test from WriteCallbackTest
+# [ RUN      ] WriteCallbackTest.AppendsDataToString
+# [       OK ] WriteCallbackTest.AppendsDataToString (0 ms)
+#  [----------] 1 test from WriteCallbackTest (0 ms total)
+#
+# [----------] Global test environment tear-down
+# [==========] 1 test from 1 test suite ran. (0 ms total)
+# [  PASSED  ] 1 test.
