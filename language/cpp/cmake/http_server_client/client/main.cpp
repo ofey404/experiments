01b001f5cf4ac4fa7997b18d6e4ae9f2fb956bcd
Ofey Chan <ofey206@gmail.com>
2023-11-18 22:50:42 +0800

    stage: http_server_client

---
+++ language/cpp/cmake/http_server_client/client/main.cpp
@@ -0,0 +1,11 @@
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+// other includes as necessary
+
+int main() {
+    // setup client, connect to server
+    // send an HTTP request to the server
+    // receive the server's response and handle it
+    // close the connection
+    return 0;
+}
---
+++ language/cpp/cmake/http_server_client/server/main.cpp
@@ -0,0 +1,11 @@
+#include <boost/asio.hpp>
+#include <boost/beast.hpp>
+// other includes as necessary
+
+int main() {
+    // setup server, listen for connections
+    // when a client connects, accept the connection and handle the request
+    // send back a response
+    // close the connection
+    return 0;
+}
