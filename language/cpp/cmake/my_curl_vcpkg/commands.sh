cdfb19123b50fee6268c85cb372b316d92e084f7
Ofey Chan <ofey206@gmail.com>
2023-11-18 15:51:05 +0800

    stage: vcpkg works

--- language/cpp/cmake/my_curl_vcpkg/commands.sh
+++ language/cpp/cmake/my_curl_vcpkg/commands.sh
@@ -12,12 +12,26 @@ cd "$SCRIPT_DIR"
 # This is a vcpkg version of ../my_curl
 
 # Install vcpkg
-sudo apt-get install build-essential tar curl zip unzip
-cd /tmp
+sudo apt-get install build-essential tar curl zip unzip pkg-config
+cd ~
 git clone https://github.com/microsoft/vcpkg
 ./vcpkg/bootstrap-vcpkg.sh
-# vcpkg package management program version 2023-10-18-27de5b69dac4b6fe8259d283cd4011e6d20a84ce
-sudo cp ./vcpkg/vcpkg /usr/local/bin
+export PATH=$PATH:$HOME/vcpkg
 vcpkg version
 # vcpkg package management program version 2023-10-18-27de5b69dac4b6fe8259d283cd4011e6d20a84ce
+
+vcpkg integrate install
+# Applied user-wide integration for this vcpkg root.
+# CMake projects should use: "-DCMAKE_TOOLCHAIN_FILE=/home/ofey/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+#####################################################################
+# dependencies
+#####################################################################
+
+# Option 1: manually install
+vcpkg install curl
+vcpkg install gtest
+
+# Option 2: use vcpkg.json
+vcpkg install
 
