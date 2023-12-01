cc430a047df9f8afd9a870564682d36acfd480ee
Ofey Chan <ofey206@gmail.com>
2023-12-01 16:35:14 +0800

    stage: +grafana

--- cloud/grafana/commands.sh
+++ cloud/grafana/commands.sh
@@ -9,17 +9,14 @@ cd "$SCRIPT_DIR"
 # Don't run. This file is a note for commands.
 ##############################################
 
-# Installation
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm install grafana grafana/grafana --version 7.0.11
 
 # check the values
-helm pull prometheus-community/prometheus --version 25.8.0 --untar
-rm -rf prometheus/
-
-helm install prometheus prometheus-community/prometheus --version 25.8.0
-kubectl port-forward svc/prometheus-server 80:80
+helm pull grafana/grafana --version 7.0.11 --untar
+rm -rf grafana/
 
 helm install grafana grafana/grafana --values grafana.yaml --version 7.0.11
+
 kubectl port-forward svc/grafana 80:80
 # password is generated
 kubectl get secret --namespace default grafana -o jsonpath="{.data.admin-password}" | base64 --decode ; echo
