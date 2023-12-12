ce34c8def33d36d93d1b40035768f6697fcd3c9c
Ofey Chan <ofey206@gmail.com>
2023-12-12 17:47:35 +0800

    stage: list

--- frontend/the-road-to-react/hacker-stories/src/App.js
+++ frontend/the-road-to-react/hacker-stories/src/App.js
@@ -1,7 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function App() {
+  const title = 'React';
+
   return (
     <div className="App">
       <header className="App-header">
@@ -15,9 +36,27 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React, Hello {title}
         </a>
       </header>
+      <div>
+        <h1>My Hacker Stories</h1>
+        <label htmlFor='search'>Search: </label>
+        <input id='search' type='text' />
+        <hr />
+        {list.map(function (item) {
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.points}</span>
+              <span>{item.num_comments}</span>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
