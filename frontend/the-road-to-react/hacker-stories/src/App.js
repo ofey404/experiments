c37a40627c0fab99baa9472f2b3816ffadd1e844
Ofey Chan <ofey206@gmail.com>
2023-12-13 18:07:00 +0800

    stage: handleChange

--- frontend/the-road-to-react/hacker-stories/src/App.js
+++ frontend/the-road-to-react/hacker-stories/src/App.js
@@ -1,31 +1,34 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-function App() {
+const App = () => {
   const title = 'React';
+  const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = event => {
     console.log(event.target.value);
+    setSearchTerm(event.target.value)
   }
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
 
   return (
     <div className="App">
@@ -47,17 +50,20 @@ function App() {
         <h1>My Hacker Stories</h1>
         <label htmlFor='search'>Search: </label>
         <input id='search' type='text' onChange={handleChange} />
+        <p>
+          Searching for <strong>{searchTerm}</strong>.
+        </p>
         <hr />
-        <List />
+        <List list={stories} />
       </div>
     </div>
   );
 }
 
-function List() {
+const List = props => {
   return (
     <div>
-      {list.map(function (item) {
+      {props.list.map(function (item) {
         return (
           <div key={item.objectID}>
             <span>
