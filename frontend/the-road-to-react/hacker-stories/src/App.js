c2a5b07ac1084e140ff22cd812c134510c6a8b87
Ofey Chan <ofey206@gmail.com>
2023-12-14 17:27:06 +0800

    stage: lift state

--- frontend/the-road-to-react/hacker-stories/src/App.js
+++ frontend/the-road-to-react/hacker-stories/src/App.js
@@ -24,9 +24,15 @@ const App = () => {
     },
   ];
 
+  const [searchTerm, setSearchTerm] = React.useState('');
   const handleSearch = event => {
       console.log(event.target.value)
+      setSearchTerm(event.target.value)
   }
+
+  const searchedStories = stories.filter(story => {
+    return story.title.toLowerCase().includes(searchTerm.toLowerCase())
+  })
 
   return (
     <div className="App">
@@ -48,29 +54,20 @@ const App = () => {
         <h1>My Hacker Stories</h1>
 
         <Search onSearch={handleSearch}/>
+
         <hr />
-        <List list={stories} />
+
+        <List list={searchedStories} />
       </div>
     </div>
   );
 }
 
 const Search = props => {
-    const [searchTerm, setSearchTerm] = React.useState('');
-
-    const handleChange = event => {
-        setSearchTerm(event.target.value)
-        props.onSearch(event)
-    }
-
   return (
       <div>
           <label htmlFor='search'>Search: </label>
-          <input id='search' type='text' onChange={handleChange} />
-          <p>
-              Searching for <strong>{searchTerm}</strong>.
-          </p>
-          <hr />
+          <input id='search' type='text' onChange={props.onSearch} />
       </div>
   )
 }
@@ -84,9 +81,9 @@ const List = props => {
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
-            <span>{item.author}</span>
-            <span>{item.points}</span>
-            <span>{item.num_comments}</span>
+            <span> {item.author}</span>
+            <span> {item.points}</span>
+            <span> {item.num_comments}</span>
           </div>
         );
       })}
