2a760d8e75e483cdfe2de72973b30af060693d8c
Ofey Chan <ofey206@gmail.com>
2023-12-15 17:53:29 +0800

    stage: before re-fetching

--- frontend/the-road-to-react/hacker-stories/src/App.js
+++ frontend/the-road-to-react/hacker-stories/src/App.js
@@ -14,32 +14,6 @@ const useSemiPersistentState = (key, initialState) => {
   return [searchTerm, setSearchTerm];
 };
 
-const initialStories = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
-
-const getAsyncStories = () =>
-  new Promise((resolve) => {
-    // throw new Error('Error thrown in getAsyncStories()');
-    // mimic a timeout
-    setTimeout(() => resolve({ data: { stories: initialStories } }), 500);
-  });
-
 const storiesReducer = (state, action) => {
   switch (action.type) {
     case 'STORIES_FETCH_INIT':
@@ -65,13 +39,15 @@ const storiesReducer = (state, action) => {
       return {
         ...state,
         data: state.data.filter(
-            story => action.payload.objectID !== story.objectID
+          (story) => action.payload.objectID !== story.objectID
         ),
       };
     default:
       throw new Error();
   }
 };
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const App = () => {
   const title = 'React';
@@ -90,15 +66,17 @@ const App = () => {
   React.useEffect(() => {
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    getAsyncStories()
+    fetch(`${API_ENDPOINT}react`)
+      .then((response) => response.json())
       .then((result) => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.data.stories,
+          payload: result.hits,
         });
       })
-      .catch((err) => dispatchStories({ type: 'STORIES_FETCH_FAILURE' })
-(err.toString()));
+      .catch((err) =>
+        dispatchStories({ type: 'STORIES_FETCH_FAILURE' })(err.toString())
+      );
   }, []);
 
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', '');
