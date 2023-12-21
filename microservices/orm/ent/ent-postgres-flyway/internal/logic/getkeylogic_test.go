c1fa9ff4b7a0b1b1e42255a6518802bd42706f82
Ofey Chan <ofey206@gmail.com>
2023-12-21 16:41:52 +0800

    stage: unit test, the right way

--- microservices/orm/ent/ent-postgres-flyway/internal/logic/getkeylogic_test.go
+++ microservices/orm/ent/ent-postgres-flyway/internal/logic/getkeylogic_test.go
@@ -11,9 +11,13 @@ import (
 )
 
 func newMockServiceContext(t *testing.T) *svc.ServiceContext {
-	// Unit test with an in-memory sqlite
+	// Unit test with an in-memory sqlite.
 	// https://github.com/ent/ent/issues/217
 	ast := assert.New(t)
+
+	// CAUTION: There could be only 1 connection to the in-memory sqlite at the same time.
+	// 			So that during svcCtx.Client.Close(), we can clean up the sqlite data.
+	//          Otherwise, there would be dirty data across the test.
 	client, err := ent.Open("sqlite3", "file:ent?mode=memory&cache=shared&_fk=1")
 	ast.Nil(err)
 
@@ -25,23 +29,28 @@ func newMockServiceContext(t *testing.T) *svc.ServiceContext {
 	}
 }
 
-func newMockGetLogic(t *testing.T, exp ...func(*svc.ServiceContext)) *GetKeyLogic {
-	svcCtx := newMockServiceContext(t)
-	for _, e := range exp {
-		e(svcCtx)
-	}
+func newMockGetLogic(t *testing.T, exp ...func(*svc.ServiceContext)) func(req *types.GetKeyReq) (resp *types.GetKeyResp, err error) {
+	return func(req *types.GetKeyReq) (resp *types.GetKeyResp, err error) {
+		svcCtx := newMockServiceContext(t)
+		for _, e := range exp {
+			e(svcCtx)
+		}
+		resp, err = NewGetKeyLogic(context.Background(), svcCtx).GetKey(req)
 
-	return NewGetKeyLogic(context.Background(), svcCtx)
+		// close the client, to clean up the sqlite data
+		assert.Nil(t, svcCtx.Client.Close())
+		return resp, err
+	}
 }
 
 func TestGetKeyLogic_GetKey(t *testing.T) {
 	ast := assert.New(t)
 	tests := []struct {
-		Name     string
-		Logic    *GetKeyLogic
-		Req      *types.GetKeyReq
-		WantResp *types.GetKeyResp
-		WantErr  error
+		Name            string
+		Logic           func(req *types.GetKeyReq) (resp *types.GetKeyResp, err error)
+		Req             *types.GetKeyReq
+		WantResp        *types.GetKeyResp
+		WantErrContains string
 	}{
 		{
 			Name: "get key",
@@ -58,6 +67,14 @@ func TestGetKeyLogic_GetKey(t *testing.T) {
 				Value: "value",
 			},
 		},
+		{
+			Name:  "key not found",
+			Logic: newMockGetLogic(t),
+			Req: &types.GetKeyReq{
+				Key: "key",
+			},
+			WantErrContains: "key not found",
+		},
 	}
 
 	for _, tt := range tests {
@@ -65,9 +82,9 @@ func TestGetKeyLogic_GetKey(t *testing.T) {
 			tt.Name,
 			func(t *testing.T) {
 				ast := assert.New(t)
-				resp, err := tt.Logic.GetKey(tt.Req)
-				if tt.WantErr != nil {
-					ast.EqualError(err, tt.WantErr.Error())
+				resp, err := tt.Logic(tt.Req)
+				if tt.WantErrContains != "" {
+					ast.ErrorContains(err, tt.WantErrContains)
 					return
 				}
 				ast.Nil(err)
