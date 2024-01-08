ee7073b706fcfe156246881e1cd29347b8caa9df
Ofey Chan <ofey206@gmail.com>
2024-01-08 11:01:41 +0800

    stage: kaniko build

--- cloud/kaniko/commands.sh
+++ cloud/kaniko/commands.sh
@@ -10,3 +10,50 @@ cd "$SCRIPT_DIR"
 ##############################################
 
 # https://github.com/GoogleContainerTools/kaniko
+
+kind create cluster -n kaniko
+docker update --restart=no kaniko-control-plane
+
+source env.sh  # see env-example.sh
+# check the credentials are valid
+docker login -u $DOCKER_USERNAME \
+             -p $DOCKER_PASSWORD \
+             $DOCKER_REGISTRY_URL
+# Login Succeeded
+
+#####################################################################
+# build from stdin, given a tarball context
+# then push to an unencrypted registry
+#####################################################################
+
+# https://github.com/GoogleContainerTools/kaniko?tab=readme-ov-file#using-standard-input
+tar -cf - Dockerfile | gzip -9 | docker run \
+  --interactive --rm -v $(pwd):/workspace gcr.io/kaniko-project/executor:v1.19.1 \
+  --context tar://stdin \
+  --destination=$DESTINATION_NOLOGIN
+# INFO[0003] Pushing image to ...
+
+docker run --pull always $DESTINATION_NOLOGIN
+# created from kaniko
+
+#####################################################################
+# push to a registry that requires login
+#####################################################################
+cat <<EOF > config.json  # see config-example.json
+{
+  "auths": {
+    "$REGISTRY_REQUIRES_LOGIN": {
+      "auth": "$(echo -n $DOCKER_USERNAME:$DOCKER_PASSWORD | base64 -w 0)"
+    }
+  }
+}
+EOF
+
+docker run \
+       -ti --rm -v $(pwd):/workspace \
+       -v $(pwd)/config.json:/kaniko/.docker/config.json:ro \
+       gcr.io/kaniko-project/executor:v1.19.1 \
+       --dockerfile=Dockerfile --destination=$DESTINATION_REQUIRES_LOGIN
+
+docker run --pull always $DESTINATION_REQUIRES_LOGIN
+# created from kaniko
---
+++ microservices/docker/commands.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# set -x             # for debug
+set -euo pipefail  # fail early
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR"
+
+##############################################
+# Don't run. This file is a note for commands.
+##############################################
+
+# always pull latest image
+docker run --pull always nginx:1.19.6-alpine
