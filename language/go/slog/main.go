8ffa96eea13ca0d13d89dc2439a6196f0359bfee
Ofey Chan <ofey206@gmail.com>
2024-01-30 18:08:34 +0800

    stage: extra key-value pairs

--- language/go/slog/main.go
+++ language/go/slog/main.go
@@ -12,6 +12,11 @@ import (
 // {"time":"2024-01-30T17:44:00.839779555+08:00","level":"INFO","msg":"Info message"}
 // {"time":"2024-01-30T17:44:00.839781549+08:00","level":"WARN","msg":"Warning message"}
 // {"time":"2024-01-30T17:44:00.839783097+08:00","level":"ERROR","msg":"Error message"}
+//
+// {"time":"2024-01-30T18:08:02.471728227+08:00","level":"DEBUG","msg":"Debug message","key":"value"}
+// {"time":"2024-01-30T18:08:02.471729814+08:00","level":"INFO","msg":"Info message","key":"value"}
+// {"time":"2024-01-30T18:08:02.471731284+08:00","level":"WARN","msg":"Warning message","key":"value"}
+// {"time":"2024-01-30T18:08:02.471732865+08:00","level":"ERROR","msg":"Error message","key":"value"}
 
 func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
@@ -22,4 +27,11 @@ func main() {
 	logger.Info("Info message")
 	logger.Warn("Warning message")
 	logger.Error("Error message")
+
+	// With some extra key-value pairs:
+	logger2 := logger.With("key", "value")
+	logger2.Debug("Debug message")
+	logger2.Info("Info message")
+	logger2.Warn("Warning message")
+	logger2.Error("Error message")
 }
