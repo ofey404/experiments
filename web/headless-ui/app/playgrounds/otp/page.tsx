0256612caaa828a44b58e753b1b23ca618fd34b7
Ofey Chan <ofey206@gmail.com>
2024-03-01 17:23:02 +0800

    stage: before useRef

--- web/headless-ui/app/playgrounds/otp/page.tsx
+++ web/headless-ui/app/playgrounds/otp/page.tsx
@@ -1,11 +1,31 @@
 'use client'
-import React, {useState} from 'react'
+import { useState, useEffect } from 'react';
 
 export default function Otp() {
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [countdown, setCountdown] = useState(60);
+
   const handleSendOTP = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     alert('OTP Sent!');
+    setIsDisabled(true);
   }
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isDisabled && countdown > 0) {
+      interval = setInterval(() => {
+        setCountdown(countdown - 1);
+      }, 1000);
+    } else if (countdown === 0) {
+      setIsDisabled(false);
+      setCountdown(60);
+    }
+    return () => {
+      clearInterval(interval);
+    };
+  }, [isDisabled, countdown]);
+
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
   };
@@ -19,8 +39,10 @@ export default function Otp() {
             <input type="text" className="text-xl border border-black m-2 rounded flex-grow" />
           </div>
           <div className="flex flex-row text-black items-center justify-center">
-            <button onClick={handleSendOTP} type="button" className="flex-grow px-4 text-xl m-2 text-white font-semibold bg-green-500 rounded">Send OTP</button>
-            <button type="submit" className="flex-grow px-4 text-xl m-2 text-white font-semibold bg-blue-500 rounded">Submit</button>
+            <button disabled={isDisabled} onClick={handleSendOTP} type="button" className={isDisabled ? "flex-grow px-4 text-xl m-2 text-white font-semibold bg-red-500 rounded" : "flex-grow px-4 text-xl m-2 text-white font-semibold bg-green-500 rounded"}>
+              {isDisabled ? `Resend OTP (${countdown})` : 'Send OTP'}
+            </button>
+            <button type="submit" className="w-32 px-4 text-xl m-2 text-white font-semibold bg-blue-500 rounded">Submit</button>
           </div>
         </form>
       </div>
