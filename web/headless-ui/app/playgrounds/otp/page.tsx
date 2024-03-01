e7261074b3e0d36a8bf1575260824bdca4139b3b
Ofey Chan <ofey206@gmail.com>
2024-03-01 17:49:18 +0800

    stage: a good enough implementation

--- web/headless-ui/app/playgrounds/otp/page.tsx
+++ web/headless-ui/app/playgrounds/otp/page.tsx
@@ -3,31 +3,38 @@ import {useState, useEffect, useRef} from 'react';
 
 export default function Otp() {
   const [requested, setRequested] = useState(false);
-  const [countdown, setCountdown] = useState(60);
-  const timer = useRef<NodeJS.Timeout>();
+  const [expiryTime, setExpiryTime] = useState<Date | null>(null);
+  const [timeLeft, setTimeLeft] = useState(60); // Store the countdown in state
+  const timer = useRef<NodeJS.Timeout | null>(null);
 
   const handleSendOTP = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     setRequested(true);
-
-    alert('OTP Sent!');
+    setExpiryTime(new Date(new Date().getTime() + 60 * 1000)); // Set expiry time to 1 minute from now
+    alert("OTP sent successfully")
   }
 
   useEffect(() => {
-    if (requested && countdown > 0) {
+    if (requested && expiryTime) {
       timer.current = setInterval(() => {
-        setCountdown(countdown - 1);
+        const now = new Date();
+        const distance = expiryTime.getTime() - now.getTime();
+        if (distance <= 0) {
+          clearInterval(timer.current!);
+          setRequested(false);
+          setExpiryTime(null);
+          setTimeLeft(60);
+        } else {
+          setTimeLeft(Math.ceil(distance / 1000)); // Update the countdown
+        }
       }, 1000);
-    } else if (countdown === 0) {
-      setRequested(false);
-      setCountdown(60);
     }
     return () => {
       if (timer.current) {
-        clearInterval(timer.current);
+        clearInterval(timer.current!);
       }
     };
-  }, [requested, countdown]);
+  }, [requested, expiryTime]);
 
   const handleSubmit = async (event: React.FormEvent) => {
     event.preventDefault();
@@ -48,7 +55,7 @@ export default function Otp() {
               type="button"
               className={`flex-grow px-4 text-xl m-2 text-white font-semibold rounded ${requested ? 'bg-red-500' : 'bg-green-500'}`}
             >
-              {requested ? `Resend OTP (${countdown})` : 'Send OTP'}
+              {requested ? `Resend OTP (${timeLeft})` : 'Send OTP'}
             </button>
             <button type="submit"
                     className="w-32 px-4 text-xl m-2 text-white font-semibold bg-blue-500 rounded">Submit
