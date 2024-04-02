f5b39258ace2f4a9205e2d3de7cbce25e9e54b0e
Ofey Chan <ofey206@gmail.com>
2024-04-02 17:51:09 +0800

    stage: gen 2

---
+++ microservices/fastapi/fastapi-example-project/scripts/gen_openapi.py
@@ -0,0 +1,17 @@
+import json
+import sys
+from pathlib import Path
+
+# Get the absolute path to the backend directory
+project_root = Path(__file__).resolve().parent.parent
+
+# Add the backend directory to the sys.path
+sys.path.append(str(project_root))
+
+from server import app
+
+schema_file = Path('openapi.json')
+with open(schema_file, 'w') as f:
+    json.dump(app.openapi(), f)
+
+print(f"Generated OpenAPI schema to {schema_file.absolute()}")
