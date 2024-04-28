787c457025f91cae1d9202b7ef7f03efd30743c6
Ofey Chan <ofey206@gmail.com>
2024-04-28 13:03:47 +0800

    stage: date

--- microservices/apache-superset/docker/pythonpath_dev/superset_config.py
+++ microservices/apache-superset/docker/pythonpath_dev/superset_config.py
@@ -92,7 +92,11 @@ class CeleryConfig:
 
 CELERY_CONFIG = CeleryConfig
 
-FEATURE_FLAGS = {"ALERT_REPORTS": True}
+FEATURE_FLAGS = {
+    "ALERT_REPORTS": True,
+    # https://superset.apache.org/docs/databases/meta-database/
+    "ENABLE_SUPERSET_META_DB ": True,
+}
 ALERT_REPORTS_NOTIFICATION_DRY_RUN = True
 WEBDRIVER_BASEURL = "http://superset:8088/"
 # The base URL for the email report hyperlinks.
