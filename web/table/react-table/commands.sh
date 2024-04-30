740cd17ac08aa64ff3eb62d5c8e9b82765f396bc
Ofey Chan <ofey206@gmail.com>
2024-04-30 16:48:02 +0800

    stage: DragDropRow

--- web/table/react-table/commands.sh
+++ web/table/react-table/commands.sh
@@ -21,26 +21,7 @@ cd react-table
 
 npm install @tanstack/react-table
 
-# https://tailwindcss.com/docs/guides/create-react-app
-npm install -D tailwindcss
-npx tailwindcss init
+# Columns with Drag and Drop - How would you implement? #3953
+# https://github.com/TanStack/table/discussions/3953
 
-# edit tailwind.config.js
-#
-# /** @type {import('tailwindcss').Config} */
-# module.exports = {
-#   content: [
-#     "./src/**/*.{js,jsx,ts,tsx}",
-#   ],
-#   ...
-# }
-#
-# edit index.css
-#
-# @tailwind base;
-# @tailwind components;
-# @tailwind utilities;
-
-# responsive table:
-#
-# overflow-x-auto
+npm install @dnd-kit/core @dnd-kit/modifiers @dnd-kit/sortable @dnd-kit/utilities @faker-js/faker
--- web/table/react-table/src/App.tsx
+++ web/table/react-table/src/App.tsx
@@ -1,11 +1,17 @@
+import ColumnVisibility from "./components/ColumnVisibility";
+import DragDropColumn from "./components/DragDropColumn";
+import DragDropRow from "./components/DragDropRow";
 import ResponsiveTable from "./components/ResponsiveTable";
 
 function App() {
   return (
     <div className="text-center bg-gray-800 min-h-screen flex flex-col items-center justify-center">
       <h1 className="text-xl text-white font-bold mb-4">React-Table Example</h1>
-      <div className='flex-col w-full px-10'>
+      <div className='flex-col w-full px-10 space-y-4'>
         <ResponsiveTable />
+        <DragDropColumn />
+        <DragDropRow />
+        <ColumnVisibility/>
       </div>
     </div>
   );
