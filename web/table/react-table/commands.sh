8cc365171a9f9bbee64393da656885c8dc16ff8f
Ofey Chan <ofey206@gmail.com>
2024-04-30 10:58:54 +0800

    stage: overflow-x-auto

--- web/table/react-table/commands.sh
+++ web/table/react-table/commands.sh
@@ -21,3 +21,26 @@ cd react-table
 
 npm install @tanstack/react-table
 
+# https://tailwindcss.com/docs/guides/create-react-app
+npm install -D tailwindcss
+npx tailwindcss init
+
+# edit tailwind.config.js
+#
+# /** @type {import('tailwindcss').Config} */
+# module.exports = {
+#   content: [
+#     "./src/**/*.{js,jsx,ts,tsx}",
+#   ],
+#   ...
+# }
+#
+# edit index.css
+#
+# @tailwind base;
+# @tailwind components;
+# @tailwind utilities;
+
+# responsive table:
+#
+# overflow-x-auto
