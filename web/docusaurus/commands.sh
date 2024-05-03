1361a3dcaf9565baf3230517dda764ff882f66c1
Ofey Chan <ofey206@gmail.com>
2024-05-03 10:01:49 +0800

    stage: docusaurus

---
+++ web/docusaurus/commands.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# set -x             # for debug
+set -euo pipefail  # fail early
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+cd "$SCRIPT_DIR"
+
+##############################################
+# Don't run. This file is a note for commands.
+##############################################
+
+# https://docusaurus.io/docs 
+
+npx create-docusaurus@latest my-website classic
+cd my-website
+npx docusaurus start
